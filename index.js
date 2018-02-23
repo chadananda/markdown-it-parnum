1a42ede4a403def752ccb9b7450ae13f027cbd05
Chad <chadananda@gmail.com>
2018-02-23 11:55:48 -0800

    fixed another one

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ module.exports = function headerSections(md) {
             pnum.section_num++
             pnum.prefix = pnum.section_num
           } 
-          console.log('Section detected: ', token, pnum, state.tokens[i+1])
+          console.log('Section detected:', token, pnum, state.tokens[i+1])
         } 
         // if (['title','subtitle','author','copyright','copy','toc', 'notoc'].filter(ex => classes.includes(ex)).length) {
         //   i++; continue;
@@ -127,7 +127,7 @@ module.exports = function headerSections(md) {
         if (!token.attrs) token.attrs = []
         token.attrs.push( ['pnum', num] )
         pnum.parnum++      
-        console.log('Par detected:', token, num, state.tokens[i+1]) 
+        console.log('Paragraph detected:', token, num, state.tokens[i+1]) 
       } 
 
       //tokens.push(token);
